f05bd90635086b015812f64d3e23b1f12540cb2c
SearyBlue <mukherjeeabhirup44@gmail.com>
2020-07-29 13:10:21 +0530

    balls falling under gravity

--- gravity.py
+++ gravity.py
@@ -6,31 +6,40 @@ from matplotlib import animation as ani
 
 
 def anim(i):
-    global xdat,ydat
-    xdat, ydat = get_dat(xdat,ydat)
+    global xdat,ydat, vx, vy
+    coll()
+    get_dat()
     plot.set_data(xdat, ydat)
+    print (vy, ydat)
     return plot,
 
-def init():
-    xdat = np.random.randint(n)
-    ydat = np.random.randint(n)
-    return xdat,ydat
+def init(n,lim):
+    xdat = np.linspace(-2*lim/3, 2*lim/3, n)
+    ydat = lim*np.random.random(n)/2 + lim/2
+    vy = np.zeros(n)
+    return xdat,ydat,vy
 
 
-def get_dat(xdat, ydat):
+def get_dat():
+    global xdat,ydat, vx, vy
+    vy = vy + g*h
+    ydat = ydat + h*vy
+
+
+def coll():
     global vy
-    vy = vy - g*h
-    ydat = ydat - h*vy
-    return xdat, ydat
+    e = 0.95
+    vy *= np.where(((ydat < 0.1) & (vy < 0)), -e, 1)   
 
 
 fig, ax = plt.subplots()
-n = 2
-g = 1
-h = 0.01
-xdat, ydat = init()
-vy = np.zeros(n)
-plot, = plt.plot(xdat,ydat)
+lim = 50
+ax.set_ylim(0,lim)
+n = 100
+g = -50000
+h = 0.001
+xdat, ydat, vy = init(n, lim)
+plot, = plt.plot(xdat,ydat, ls=' ', marker='o')
 
 
 myani = ani.FuncAnimation(fig, anim, interval=1000*h)
